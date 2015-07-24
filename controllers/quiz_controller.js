1b1f1ce58c585160a2f89b09dbaf0a31b687fdd5
davidmorenoros <moreno.varios@gmail.com>
2015-07-24 14:18:18 +0200

    Final Modulo 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -42,6 +42,8 @@ exports.answer = function(req,res){
 };
 // FIN: CON BBDD
 */
+
+/*
 var models = require('../models/models.js');
 //GET /quizes/question
 
@@ -61,6 +63,53 @@ exports.answer = function(req, res){
 			res.render('quizes/answer',{respuesta: 'Correcto'});
 		} else {
 			res.render('quizes/answer',{respuesta: 'Incorrecto'});
+		}
+	})
+};
+
+*/
+
+// Lista de Preguntas y Autoload
+var models = require('../models/models.js');
+
+//Autoload - factoriza el codigo si ruta incluye :quizId
+exports.load = function(req, res, next, quizId){
+	models.Quiz.findById(quizId).then(
+		function(quiz){
+			if(quiz){
+				req.quiz = quiz;
+				next();
+			} else{
+				next( new Error('No existe quizID=' + quizId));
+			}
+		}
+		).catch(function(error){ next(error);});
+};
+
+//GET /quizes
+exports.index = function(req, res){
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index.ejs',{quizes: quizes});
+	}).catch(function(error){next(error);});
+};
+
+
+//GET /quizes/:id
+exports.show = function(req, res){
+	//console.log('entra en el controller de quiz en preguntas');
+	//console.log('req.params.quizId: ' + req.params.quizId);	
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
+		res.render('quizes/show', {quiz: req.quiz});
+	})
+};
+
+//GET /quizes/:id/answer
+exports.answer = function(req, res){
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
+		if(req.query.respuesta === req.quiz.respuesta){
+			res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Correcto'});
+		} else {
+			res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Incorrecto'});
 		}
 	})
 };
--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,33 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'QuizDMR' });
 });
 
-/* Question page. */
-/*router.get('/quizes/question', function(req,res){
-  res.render('./quizes/question',{ title: 'Question - QuizDMR' });
-});*/
+/*
+// Question page.
+//router.get('/quizes/question', function(req,res){
+//  res.render('./quizes/question',{ title: 'Question - QuizDMR' });
+//});
 router.get('/quizes/question', quizController.question);
 
-/* Answer page. */
-/*router.get('/answer', function(req,res){
-  res.render('./quizes/answer', { title: 'Answer - QuizDMR'});
-});*/
+// Answer page.
+//router.get('/answer', function(req,res){
+//  res.render('./quizes/answer', { title: 'Answer - QuizDMR'});
+//});
 router.get('/quizes/answer', quizController.answer);
+*/
 
-/* Credits page. */
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
+// Lista de preguntas - Definición de rutas de /quizes
+// Index
+router.get('/quizes', quizController.index);
+// Question page.
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+// Answer page.
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
+
+// Credits page.
 router.get('/author', function(req,res){
   res.render('./author', { title: 'Credits'});
 });
